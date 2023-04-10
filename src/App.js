6f3c1d91924317a70bca428ad3d5fe1d6326661c
ShubhamKumar977 <78530993+ShubhamKumar977@users.noreply.github.com>
2023-04-10 14:46:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Wolld! Prashant kahe he Paresan!!!!!!!!</h1>
+      <h1>Hello World! Prashant kahe he Paresan!!!!!!!!</h1>
     </div>
   );
 }
