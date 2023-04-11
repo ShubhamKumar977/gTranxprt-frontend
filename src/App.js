0735bd9267d2ebcccf2910a7dd626fed65dcc4be
Prashant Singh Negi <prashantsinghnegi7@gmail.com>
2023-04-11 11:06:18 +0530

    Add layout component, signup form, routing and .gitignore file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is Prashant</h1>
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path="/signup"
+            element={
+              <>
+                <Signup />
+              </>
+            }
+          />
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                {' '}
+                <Home />{' '}
+              </>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/layout.css';
+
+import React from 'react';
+
+export default function Layout({ children }) {
+  return (
+    <div className="layout-container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="navbar-container">
+          <div className="navbar-icon">
+            <a className="navbar-brand" href="/">
+              GTranscriptor
+            </a>
+          </div>
+
+          <div className="navbar-menu">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="/signup">
+                  Sign Up
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <main>{children}</main>
+    </div>
+  );
+}
