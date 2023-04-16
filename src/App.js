e11143cd99aacc7d31bab7573e8f020a1f21fe3d
ShubhamKumar977 <78530993+ShubhamKumar977@users.noreply.github.com>
2023-04-16 19:19:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           />
           <Route
             exact
-            path="/gTranxprt-frontend"
+            path="/"
             element={
               <>
                 {' '}
