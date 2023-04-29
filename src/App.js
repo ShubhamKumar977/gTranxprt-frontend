330d29088ddb0a21596cbe855b220f707ec2759c
ShubhamKumar977 <78530993+ShubhamKumar977@users.noreply.github.com>
2023-04-30 00:42:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
             element={
               <>
                 {' '}
-                <Home/>{' '}
+                <Home />
               </>
             }
           />
